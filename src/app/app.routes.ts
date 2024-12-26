9fe42ff2534715a616efae2295fe04f4f4c468f0
Priti <pritikhandare1997@gmail.com>
2024-12-26 13:17:26 +0530

    Add Site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
+import { MyOrdersComponent } from './pages/my-order/my-order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path:'home',
+        component: ProductsComponent
+    },
+    {
+        path:'create-order',
+        component:CreateOrderComponent 
+    },
+    {
+        path:'my-order',
+        component:MyOrdersComponent 
+    }
+
+];
